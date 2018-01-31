16661de38771fc9a6f2bd6ca83e3f2f1d33e904e
Jane Lee <Yesdoing13@gmail.com>
2018-01-31 18:59:37 +0900

    Customizing the Django Admin

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from nomadgram.users import models as user_models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
-
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,6 +12,7 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
 
     """ Image Model """
@@ -20,7 +22,10 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.locations, self.caption)
 
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
 
     """ Comment Model """
@@ -29,9 +34,16 @@ class Comment(TimeStampedModel):
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
 
+    def __str__(self):
+        return self.message
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
 
     """ Like Model """
 
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
