70172687e944d071582a5b9874b57663b3a1d484
Jane Lee <Yesdoing13@gmail.com>
2018-03-05 15:44:48 +0900

    Searching by Hashtag

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from nomadgram.users import models as user_models
+from taggit.managers import TaggableManager
 from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
@@ -21,6 +22,7 @@ class Image(TimeStampedModel):
     locations = models.CharField(max_length=140)
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True, related_name='images')
+    tags = TaggableManager()
 
     @property
     def like_count(self):
